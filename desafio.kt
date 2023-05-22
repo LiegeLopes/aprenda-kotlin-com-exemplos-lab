858b4d52d339b592032aad89ea69d93ef0aebf01
LiegeLopes <34957712+LiegeLopes@users.noreply.github.com>
2023-05-22 19:38:36 -0300

    Melhoria de codigo

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,34 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 120)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 180)
+
+    val formacao = Formacao("Formação Kotlin")
+    formacao.conteudos.add(conteudo1)
+    formacao.conteudos.add(conteudo2)
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    println("Usuários matriculados: ${formacao.inscritos.size}")
+    println("Conteúdos da formação:")
+    for (conteudo in formacao.conteudos) {
+        println(conteudo.nome)
+    }
 }
